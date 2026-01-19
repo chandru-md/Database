c741ea3a20be3023f11c3f3644284262f60beaa9
chandru-md <chandrumd2071@gmail.com>
2026-01-19 19:15:04 +0530

    Intial Commit

---
+++ Learning_Creating_Database_Tables.sql
@@ -0,0 +1,93 @@
+create database ChandruCafe;
+
+use ChandruCafe;
+
+
+create database kathiravanCafe;
+
+show databases;
+
+
+drop database ChandruCafe;
+
+
+show databases;
+
+
+create database chandrucafe;
+
+use chandrucafe;
+
+create table foods(
+SNO int auto_increment primary key,
+food_name varchar(100),
+price int 
+);
+
+
+select * from foods;
+
+
+show tables;
+
+
+show columns from foods;
+
+
+show tables;
+
+
+desc foods;
+
+
+create table fruits(
+SN0 int primary key auto_increment,
+juice_name varchar(100),
+price int
+);
+
+
+show tables;
+
+
+desc fruits;
+
+show tables;
+
+desc foods;
+desc fruits;
+
+
+drop table foods;
+
+
+show tables;
+
+
+drop table fruits;
+
+show tables;
+
+
+
+create table pastries(
+name varchar(100),
+quantity int
+);
+
+
+show tables;
+
+
+desc pastries;
+
+drop table pastries;
+
+
+show tables;
+
+
+-- Comments
+
+
+-- My name is Chandru, I'm Learning Ai and Data science
