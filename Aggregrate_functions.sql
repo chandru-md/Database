2312373faca5b36a6148225734a47132e57e758b
chandru-md <chandrumd2071@gmail.com>
2026-01-23 00:40:32 +0530

    updated -- aggregate functions

--- Aggregrate_functions.sql
+++ Aggregrate_functions.sql
@@ -75,3 +75,70 @@ select released_year,count(*) as books_published
  select max(pages) from books;
  
  select max(pages) ,title from books;
+ 
+ select * from books where pages=634;
+ 
+ select title,pages from books order by pages desc limit 1;
+ 
+ select title,pages from books order by pages asc limit 1;
+ 
+ select title,pages from books;
+ 
+ select title,pages from books where pages = (select max(pages) from books);
+ 
+select title,pages from books where pages = (select min(pages) from books)
+ 
+INSERT INTO books (title, pages)
+VALUES ("chandru's bio-graphy", 1090);
+
+desc books;
+ 
+ select * from books;
+
+
+select title,pages from books where pages = (select max(pages) from books);
+
+-- grouping by multiple columns
+
+select author_fname,author_lname from books;
+
+select author_fname,author_lname from books order by author_lname;
+
+select author_lname,count(*) from books group by author_lname;
+
+select author_fname,author_lname, count(*) from books group by author_lname,author_fname;
+
+select concat(author_fname,' ',author_lname) as author ,count(*) from books group by author;
+
+-- min/max -- group-by
+
+desc books;
+
+select author_lname,min(released_year) from books group by author_lname;
+
+select concat(author_fname,' ',author_lname) as author,
+count(*) as books_written,
+min(released_year) as earliest_release,
+max(released_year) as latest_release,
+max(pages) as longest_Book
+from books group by author;
+
+-- sum
+
+-- add things together
+
+select sum(pages) from books;
+
+select author_lname, pages from books;
+
+select author_lname, count(*), sum(pages) from books
+group by author_lname;
+
+-- average
+
+select avg(released_year) from books;
+
+select avg(pages) from books;
+
+select avg(stock_quantity) from books;
+
