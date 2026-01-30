b2ec72af1e0aac448cc443488eb5521b3da1ba9b
chandru-md <chandrumd2071@gmail.com>
2026-01-30 23:53:02 +0530

    One-to-many-and-joins-updated.

---
+++ practice_7..sql
@@ -0,0 +1,13 @@
+-- DATA IS MESSY
+
+select database();
+
+show databases;
+
+use book_shop;
+
+select * from books;
+
+-- 1.One to One relationship
+-- 2.One to Many relationship
+-- 3.Many to Many Relationship
